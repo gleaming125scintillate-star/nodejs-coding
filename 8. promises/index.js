3acb9dfeee0851679ddd2be73e4eaec3457ddb89
Ashish Raj <gleaming125.scintillate@gmail.com>
2026-05-20 19:21:55 +0530

    upto async

---
+++ 8. promises/index.js
@@ -0,0 +1,29 @@
+// function delayFn(time) {
+//   return new Promise((resolve, reject) => setTimeout(resolve, time));
+// }
+
+// console.log("Promise Lecture starts");
+// delayFn(2000).then(() => console.log("after 2 seconds promise resolved"));
+// console.log("End");
+
+function delayFn(time) {
+  return new Promise((resolve, reject) => setTimeout(resolve, time));
+}
+
+console.log("Promise Lecture starts");
+delayFn(2000).then(() => console.log("after 2 seconds promise resolved"));
+console.log("End");
+
+function divideFn(num1, num2) {
+  return new Promise((resolve, reject) => {
+    if (num2 === 0) {
+      reject("Can not perform division by 0");
+    } else {
+      resolve(num1 / num2);
+    }
+  });
+}
+
+divideFn(10, 0)
+  .then((result) => console.log(result, "res"))
+  .catch((error) => console.log(error, "err"));
